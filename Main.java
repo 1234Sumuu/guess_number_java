64aa991dcf224f77b75c0bfd043119e7d9167a2b
1234Sumuu <41938215+1234Sumuu@users.noreply.github.com>
2022-07-12 23:19:55 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int myNumber = (int) (Math.random() * 100);
+        int userNumber = 0;
+
+        do {
+            System.out.println("Guess my number: ");
+            userNumber = sc.nextInt();
+
+            if (userNumber == myNumber) {
+                System.out.println("Yeeeaaaa..... The Number is Correct!");
+                break;
+            }else if(userNumber>myNumber){
+                System.out.println("Your Number is too large!");
+            }else{
+                System.out.println("Your Number is too small..");
+            }
+        }while(userNumber >= 0);
+
+        System.out.println("My number was : ");
+        System.out.println(myNumber);
+    }
+}
